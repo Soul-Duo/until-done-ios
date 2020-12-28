0716dd6ad923a3aa3dcea890c9a854b9b44c10da
kevinkim2586 <44637101+kevinkim2586@users.noreply.github.com>
2020-12-28 16:42:50 +0900

    added View Controller to display info about the given task

---
+++ UntilDone/UntilDone/TaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TaskViewController.swift
+//  UntilDone
+//
+//  Created by Kevin Kim on 2020/12/28.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
